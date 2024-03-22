85c7d103f7416f1ea08de2a526f414dbee5b4464
Dijana Damchevska <77120285+damcevskad@users.noreply.github.com>
2024-03-22 14:40:05 +0100

    Create Trial3.cs

---
+++ Trial3.cs
@@ -0,0 +1,49 @@
+try
+{
+    Console.Write("Number of arrays: ");
+    int array_num = int.Parse(Console.ReadLine());
+
+    int[] multipliers = new int[array_num];
+
+    for (int i = 0; i < array_num; i++)
+    {
+        Console.Write($"\nLength of array {i + 1}: ");
+        int array_len = int.Parse(Console.ReadLine());
+
+        int[] array = new int[array_len];
+
+        Console.WriteLine($"\nArray elements:");
+        for (int e = 0; e < array_len; e++)
+        {
+            array[e] = int.Parse(Console.ReadLine());
+        }
+
+        Console.WriteLine("\nArray: ");
+        for (int e = 0; e < array_len; e++)
+        {
+            Console.Write(array[e] + " ");
+        }
+        Console.WriteLine();
+
+        multipliers[i] = array[i];
+
+        Console.WriteLine("\nMultipliers: ");
+        for (int j = 0; j < multipliers.Length; j++)
+        {
+            Console.Write(multipliers[j] + " ");
+        }
+        Console.WriteLine();
+    }
+
+    int product = 1;
+    foreach (int multiplier in multipliers)
+    {
+        product *= multiplier;
+    }
+
+    Console.WriteLine($"\nProduct: {product}");
+}
+catch(FormatException f)
+{
+    Console.WriteLine($"Format exception: {f}");
+}
